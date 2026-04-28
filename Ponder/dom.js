9758d5f2baed291b9c43078cc087f920204719f1
Asa Ryan Fletcher <167818887+AsaRy13@users.noreply.github.com>
2026-04-28 10:02:19 -0600

    Ponder stuff

--- Ponder/dom.js
+++ Ponder/dom.js
@@ -7,3 +7,14 @@ selectElem.addEventListener('change', function(){
 
 let firstList = document.querySelector(".list");
 firstList.style.backgroundColor = "red";
+
+let content = document.getElementById("content");
+content.style.backgroundColor = "blue";
+
+document.querySelector("img").setAttribute("src", "byui-logo-blue.webp");
+document.querySelector("img").setAttribute("alt", "New Image.");
+
+document.querySelector("h1").textContent = "MONSTER TRUCKS!!!";
+document.getElementsByTagName("h1")[0].innerText = "MONSTER TRUCKS!22!!";
+
+console.log(document.getElementById("content"));
