cca474a67e9e8b1bb16dfbdcd5e54254ea7565f6
Asa Ryan Fletcher <167818887+AsaRy13@users.noreply.github.com>
2026-04-30 20:34:57 -0600

    logo fix

--- mission/mission.js
+++ mission/mission.js
@@ -9,15 +9,11 @@ function changeMode() {
         document.body.style.color = "white";
         document.querySelector("h4").style.color = "#035f9c";
         document.getElementById("logo").setAttribute("src", "byui-logo-white.png");
-        document.getElementById("logo").style.height = "100%";
-        document.getElementById("logo").style.width = "100%";
     }
     else{
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
         document.querySelector("h4").style.color = "#035f9c";
         document.getElementById("logo").setAttribute("src", "byui-logo-blue.webp");
-        document.getElementById("logo").style.height = "25%";
-        document.getElementById("logo").style.width = "25%";
     }
 }
