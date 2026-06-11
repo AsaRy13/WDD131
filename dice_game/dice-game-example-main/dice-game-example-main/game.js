73f9174b41b8b904d5a765ceb7623ba5a669011e
Asa Ryan Fletcher <167818887+AsaRy13@users.noreply.github.com>
2026-06-11 09:58:15 -0600

    Added game.html

---
+++ dice_game/dice-game-example-main/dice-game-example-main/game.js
@@ -0,0 +1,26 @@
+const dieImages = document.querySelectorAll("#gameboard img")
+// when button is pressed
+// then change die images to gif animation
+
+document.getElementById("rollButton").addEventListener("click", (event) => {
+    dieImages.forEach((image) => {
+        if(isDieUnlocked(image)){
+            image.src = "assets/die_rolling.gif";
+        }
+    });
+
+    setTimeout(() => {
+        // set all die images to a random number
+        dieImages.forEach((image) => {
+            if(isDieUnlocked(image)){
+                image.src = `assets/white_dice_${Math.floor(Math.random() * 6) + 1}.gif`;
+            }
+        });
+    }, 500);
+});
+
+function isDieUnlocked(dieImage) {
+    const checkboxes = document.querySelectorAll("#gameboard input");
+    const unchecked = Array.from(checkboxes).filter(checkbox => !checkbox.checked);
+    return unchecked.find(checkbox => checkbox.className === dieImage.className);
+}
